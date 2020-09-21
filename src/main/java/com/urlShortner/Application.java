735c5ae8b5a9db9c439a083759d0cd4935cedd24
Deepak1jha <ddjha171@gmail.com>
2020-09-21 14:30:08 +0530

    First commit

---
+++ src/main/java/com/urlShortner/Application.java
@@ -0,0 +1,20 @@
+package com.urlShortner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		log.info("Statrtf");
+		log.info("Statrtf");
+		log.info("Statrtf");
+		log.info("Statrtf");
+		log.info("Statrtf");
+	}
+
+}
